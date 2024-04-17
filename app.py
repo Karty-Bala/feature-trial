b45dedd18b89189c89599a949b717d1ddf0df121
Karty-Bala <sit20co029@sairamtap.edu.in>
2024-04-17 20:45:34 +0530

    feature trial commit 1

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import pickle
 import warnings
 
 warnings.filterwarnings('ignore')
-
 file = open("pickle/model.pkl", "rb")
 gbc = pickle.load(file)
 file.close()
@@ -24,6 +23,12 @@ def get_domain_name(url):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+
+    webCatURL = ("https://website-categorization.whoisxmlapi.com/api/v3?apiKey=at_Xvchfwv7SVhPs9JPN0he6PVzzZPge"
+                 "&domainName=")
+    geoLocURL = "https://ip-geolocation.whoisxmlapi.com/api/v1?apiKey=at_Xvchfwv7SVhPs9JPN0he6PVzzZPge&domain="
+    domRepURL = "https://domain-reputation.whoisxmlapi.com/api/v2?apiKey=at_Xvchfwv7SVhPs9JPN0he6PVzzZPge&domainName="
+
     if request.method == "POST":
         url = request.form["url"]
         obj = FeatureExtraction(url)
@@ -34,24 +39,45 @@ def index():
         y_pro_non_phishing = gbc.predict_proba(x)[0, 1]
 
         domain_name = get_domain_name(url)
-        result = requests.get("https://website-categorization.whoisxmlapi.com/api/v3?apiKey=at_Xvchfwv7SVhPs9JPN0he6PVzzZPge&domainName=" + domain_name)
-        data = json.loads(result.content.decode('ascii'))
-        categories = data.get('categories', [])
-        print(data)
-        print(categories)
-        if 'code:422' in data:
-            listCategories = [{"name": "Unavailable", "confidence": "Not Enough Content"}]
+
+        webCatResult = requests.get(webCatURL + domain_name)
+        geoLocResult = requests.get(geoLocURL + domain_name)
+        domRepResult = requests.get(domRepURL + domain_name)
+
+        webCat_data = json.loads(webCatResult.content.decode('ascii'))
+        geoLoc_data = json.loads(geoLocResult.content.decode('ascii'))
+        domRep_data = json.loads(domRepResult.content.decode('ascii'))
+
+        # webCat INFO
+        categories = webCat_data.get('categories', [])
+        if 'code:422' in webCat_data:
+            listCategories = [{"name": "", "confidence": "Not Enough Content"}]
         else:
             listCategories = []
-
             for category in categories:
                 name = category.get('name', '')
                 confidence = category.get('confidence', 0)
                 listCategories.append({"name": name, "confidence": int(confidence * 100)})
 
-            print(listCategories)
+        #geoloc INFO
+        country = geoLoc_data["location"]["country"]
+        region = geoLoc_data["location"]["region"]
 
-        return render_template('index.html', xx=round(y_pro_non_phishing, 2), url=url, categories=categories)
+        #domRep INFO
+        warning_descriptions = []
+        domRepScore = domRep_data["reputationScore"]
+        for index, result in enumerate(domRep_data["testResults"]):
+            if result["test"] == "SSL vulnerabilities":
+                for warning in result["warnings"]:
+                    warning_descriptions.append(warning["warningDescription"])
+                break
+        print(listCategories)
+        print("Country:", country)
+        print("Region:", region)
+        print("Domain Reputation Score:", domRepScore)
+        for warns in warning_descriptions:
+            print(warns)
+        return render_template('index.html', xx=round(y_pro_non_phishing, 2), url=url, listCategories=listCategories, country=country, region=region, domRepScore=domRepScore, warning_descriptions=warning_descriptions)
     return render_template("index.html", xx=-1)
 
 
--- trial_requests.py
+++ trial_requests.py
@@ -1,5 +1,48 @@
+import json
+from urllib.parse import urlparse
 import requests
 
+
+def get_domain_name(url):
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc
+    return domain
+
+
 url = str(input("enter url: "))
-result = requests.get("https://website-categorization.whoisxmlapi.com/api/v2?apiKey=at_Xvchfwv7SVhPs9JPN0he6PVzzZPge&domainName=" + url)
-print((result.content).decode('ascii'))
+domain_name = get_domain_name(url)
+print(domain_name)
+webCatURL = "https://website-categorization.whoisxmlapi.com/api/v2?apiKey=at_Xvchfwv7SVhPs9JPN0he6PVzzZPge&domainName="
+geoLocURL = "https://ip-geolocation.whoisxmlapi.com/api/v1?apiKey=at_Xvchfwv7SVhPs9JPN0he6PVzzZPge&domain="
+domRepURL = "https://domain-reputation.whoisxmlapi.com/api/v2?apiKey=at_Xvchfwv7SVhPs9JPN0he6PVzzZPge&domainName="
+
+domRepresult = requests.get( domRepURL + domain_name)
+domRep_data = json.loads(domRepresult.content.decode('ascii'))
+print(domRep_data)
+
+warning_descriptions = []
+repScore = domRep_data["reputationScore"]
+ssl_vulnerabilities_index = None
+for index, result in enumerate(domRep_data["testResults"]):
+    if result["test"] == "SSL vulnerabilities":
+        ssl_vulnerabilities_index = index
+        # Append warning descriptions to the list
+        for warning in result["warnings"]:
+            warning_descriptions.append(warning["warningDescription"])
+        break
+print(warning_descriptions)
+# ssl_warnings = domRep_data["testResults"][2]["warnings"]  # Assuming SSL vulnerabilities are always at index 2
+# domRepList = []
+# print("Warning Descriptions of SSL Vulnerabilities:")
+# for warning in ssl_warnings:
+#     # print("-", warning["warningDescription"])
+#     domRepList.append(warning["warningDescription"])
+# rep_score = domRep_data["reputationScore"]
+
+# geoLocresult = requests.get( geoLocURL + domain_name)
+# loca_data = json.loads(geoLocresult.content.decode('ascii'))
+# # print((result.content).decode('ascii'))
+#
+# print("Country:", loca_data["location"]["country"])
+# print("Region:", loca_data["location"]["region"])
+
